d9f1884edc1de1a3fd8b1ad1060e1598297c7d5e
mayra <carvalhomaeb@gmail.com>
2026-02-16 14:03:56 -0300

    adicionado exercicio 10

--- exercicios.py
+++ exercicios.py
@@ -1,7 +1,15 @@
+import math
+
 # Faça um programa que peça dois números inteiros e imprima a divisão do primeiro pelo segundo.
-numero_01 = int(input("Inserir um nuemro inteiro: "))
-numero_02 = int(input("Inserir um nuemro inteiro: "))
+numero_01 = int(input("Inserir um numero inteiro: "))
+numero_02 = int(input("Inserir um numero inteiro: "))
 
 resultado = numero_01 // numero_02
 
 print(resultado)
+
+# Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
+
+raio_do_circulo = float(input("Digite o raio: "))
+area_do_circulo = math.pi * raio_do_circulo ** 2
+print(f"{area_do_circulo:.2f}")
